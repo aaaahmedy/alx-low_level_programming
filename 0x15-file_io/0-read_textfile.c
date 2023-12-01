d3ab15f12135f6626fe509727f0acf33314c8a60
yassine <yassinelamnaiy@gmail.com>
2023-12-01 22:00:30 +0100

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /*
  *read_textfile : reads a text file and prints
@@ -8,41 +9,41 @@
  *
 */
 
-  ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	int rd , wr;
+	int rd, wr;
 	int fd;
 
 
 	if (filename == NULL)
-		return 0;
+		return (0);
 
-	fd = open("filename",O_RDONLY);
+	fd = open("filename", O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 
-	buffer = (char*)malloc(sizeof(char)*letters);
+	buffer = (char *)malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return 0;
+		return (0);
 
-	rd = read(fd,buffer,letters);
+	rd = read(fd, buffer, letters);
 	if (rd == -1)
 	{
 		free(buffer);
-		return 0;
+		return (0);
 	}
 
-	wr = write(STDOUT_FILENO,buffer,rd);
+	wr = write(STDOUT_FILENO, buffer, rd);
 	if (wr == -1)
 	{
 		free(buffer)
-		return 0;
+		return (0);
 	}
 
 	close(fd);
 
 	free(buffer);
 	buffer = NULL;
-	return wr;
+	return (wr);
 }
