3eed897762fb499b611ac12d2dc83d937052064e
yassine <yassinelamnaiy@gmail.com>
2023-12-01 22:06:33 +0100

    my commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,22 +11,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
 	ssize_t rd, wr;
 	int fd;
-
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
-	buffer = (char *)malloc(sizeof(char) * letters);
+	buffer = (char *)malloc(sizeof(char *) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	rd = read(fd, buffer, letters);
 	if (rd == -1)
 	{
@@ -37,13 +33,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	wr = write(STDOUT_FILENO, buffer, rd);
 	if (wr == -1)
 	{
-		free(buffer)
+		free(buffer);
 		return (0);
 	}
-
 	close(fd);
 
 	free(buffer);
-	buffer = NULL;
 	return (wr);
 }
